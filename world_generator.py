89f764f11caa5ade1c79dd7fc0211cf91e8f4839
Naël <nael.soufi@gmail.com>
2023-07-09 18:30:42 +0200

    Generate and store tavern in the database This version has neo4j database set up in order to store taverns that have been generated

--- world_generator.py
+++ world_generator.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template, request
+from py2neo import Graph
 import classes
 
 # Setting up the app
 app = Flask(__name__)
 
-# Disable template caching
-app.debug = True
+# Create a Neo4j driver and session
+graph = Graph("bolt://localhost:7687", auth=("neo4j", "nanou9673"))
 
 # Setting up a root : when going to / this is what happen
 @app.route('/')
 def hello_world():
+    # Generate the tavern object
     tavern = classes.Tavern()
+
+    # Prepare the query with $placeholders
+    query = "CREATE (tavern:BUILDING {{name: '{tavern_name}', type: 'tavern'}}) -[:OWNED_BY]-> (owner:NPC {{name: '{npc_name}', gender: '{npc_gender}' }})".format(tavern_name=tavern.name, npc_name=tavern.owner['name'], npc_gender=tavern.owner['gender'])
+
+    graph.run(query)
+
     return render_template('index.html', tavern=tavern)
 
 @app.route('/npc/<npc_name>')
